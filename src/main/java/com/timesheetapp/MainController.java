3718cc5bd0a64f3f39ef0e7435ce52dbc50537f4
Radek Pelikán <pelikan.radek03@gmail.com>
2023-04-15 01:05:06 +0200

    Added month changing buttons

--- src/main/java/com/timesheetapp/MainController.java
+++ src/main/java/com/timesheetapp/MainController.java
@@ -9,40 +9,34 @@ import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class MainController implements Initializable {
 
     @FXML
     public GridPane gridPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Grid has 7 columnds and 6 rows, populate every cell with a label x and y position
+        populateGrid();
 
+    }
 
+    private void populateGrid() {
         int counter = 1;
+        // 7 Columns
         for (int x = 0; x < 7; x++) {
+            // 6 Rows (0 is the header)
             for (int y = 1; y < 7; y++) {
                 Pane pane = new Pane();
+                pane.getStyleClass().add("cal-item");
 
-                pane.setPrefSize(100, 100);
-                GridPane.setMargin(pane, new javafx.geometry.Insets(5));
-
-                pane.setStyle("-fx-border-color: black;");
+                GridPane.setHgrow(pane, javafx.scene.layout.Priority.ALWAYS);
+                GridPane.setVgrow(pane, javafx.scene.layout.Priority.ALWAYS);
+                GridPane.setMargin(pane, new javafx.geometry.Insets(1));
 
                 Label label = new Label(String.valueOf(counter++));
                 label.prefWidthProperty().bind(pane.widthProperty());
                 label.prefHeightProperty().bind(pane.heightProperty());
                 label.setAlignment(javafx.geometry.Pos.CENTER);
-
-                // On hover color the pane black and set labels text color to white
-                pane.setOnMouseEntered(e -> {
-                    pane.setStyle("-fx-background-color: black;");
-                    label.setTextFill(javafx.scene.paint.Color.WHITE);
-                });
-                pane.setOnMouseExited(e -> {
-                    pane.setStyle("-fx-background-color: white; -fx-border-color: black;");
-                    label.setTextFill(javafx.scene.paint.Color.BLACK);
-                });
 
                 pane.getChildren().add(label);
                 gridPane.add(pane, x, y);
