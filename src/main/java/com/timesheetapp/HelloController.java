3a13a46608f53e97e944f6a2b1fecf4fe931c611
Radek Pelikán <pelikan.radek03@gmail.com>
2023-04-15 00:05:06 +0200

    Styling and populating calendar  with numbers

--- src/main/java/com/timesheetapp/HelloController.java
+++ src/main/java/com/timesheetapp/HelloController.java
@@ -1,14 +1,52 @@
 package com.timesheetapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public GridPane gridPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Grid has 7 columnds and 6 rows, populate every cell with a label x and y position
+
+
+        int counter = 1;
+        for (int x = 0; x < 7; x++) {
+            for (int y = 1; y < 7; y++) {
+                Pane pane = new Pane();
+
+                pane.setPrefSize(100, 100);
+                GridPane.setMargin(pane, new javafx.geometry.Insets(5));
+
+                pane.setStyle("-fx-border-color: black;");
+
+                Label label = new Label(String.valueOf(counter++));
+                label.prefWidthProperty().bind(pane.widthProperty());
+                label.prefHeightProperty().bind(pane.heightProperty());
+                label.setAlignment(javafx.geometry.Pos.CENTER);
+
+                // On hover color the pane black and set labels text color to white
+                pane.setOnMouseEntered(e -> {
+                    pane.setStyle("-fx-background-color: black;");
+                    label.setTextFill(javafx.scene.paint.Color.WHITE);
+                });
+                pane.setOnMouseExited(e -> {
+                    pane.setStyle("-fx-background-color: white; -fx-border-color: black;");
+                    label.setTextFill(javafx.scene.paint.Color.BLACK);
+                });
+
+                pane.getChildren().add(label);
+                gridPane.add(pane, x, y);
+            }
+        }
     }
 }
