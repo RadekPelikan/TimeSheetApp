5cb2bacfbd7137ad47971f5acdd15a723091f6c2
Radek Pelikán <pelikan.radek03@gmail.com>
2023-04-15 01:55:57 +0200

    Populating grid and changing months

--- src/main/java/com/timesheetapp/MainController.java
+++ src/main/java/com/timesheetapp/MainController.java
@@ -1,46 +1,138 @@
 package com.timesheetapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
     @FXML
     public GridPane gridPane;
+    @FXML
+    public Label monthLabel;
+
+    private int moved = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        populateGrid();
+        LocalDate date = LocalDate.now();
 
+        ArrayList<DateItem> month = createMonth(date);
+        populateCalendar(month);
     }
 
-    private void populateGrid() {
-        int counter = 1;
-        // 7 Columns
-        for (int x = 0; x < 7; x++) {
-            // 6 Rows (0 is the header)
-            for (int y = 1; y < 7; y++) {
-                Pane pane = new Pane();
-                pane.getStyleClass().add("cal-item");
+    private ArrayList<DateItem> createMonth(LocalDate date) {
+        Month month = Month.of(date.getMonthValue());
+        String monthName = month.getDisplayName(java.time.format.TextStyle.SHORT, java.util.Locale.ENGLISH);
+        int year = date.getYear();
+        monthLabel.setText(String.format("%s %d", monthName, year));
 
-                GridPane.setHgrow(pane, javafx.scene.layout.Priority.ALWAYS);
-                GridPane.setVgrow(pane, javafx.scene.layout.Priority.ALWAYS);
-                GridPane.setMargin(pane, new javafx.geometry.Insets(1));
+        // Get me a first day of the month
+        int firstDayOfMonth = date.withDayOfMonth(1).getDayOfWeek().getValue();
 
-                Label label = new Label(String.valueOf(counter++));
-                label.prefWidthProperty().bind(pane.widthProperty());
-                label.prefHeightProperty().bind(pane.heightProperty());
-                label.setAlignment(javafx.geometry.Pos.CENTER);
-
-                pane.getChildren().add(label);
-                gridPane.add(pane, x, y);
-            }
+        int daysInMonth = month.length(date.isLeapYear());
+        ArrayList<DateItem> dateItems = new ArrayList<>();
+        for (int i = firstDayOfMonth; i < firstDayOfMonth + daysInMonth; i++) {
+            dateItems.add(new DateItem(i, "ahoj"));
         }
+        return dateItems;
+    }
+
+    private void move(int amount) {
+        LocalDate date = LocalDate.now();
+        date = date.plusMonths(moved + amount);
+
+        ArrayList<DateItem> month = createMonth(date);
+        populateCalendar(month);
+    }
+
+    public void onForward(ActionEvent actionEvent) {
+        move(++moved);
+    }
+    public void onBackward(ActionEvent actionEvent) {
+        move(--moved);
+    }
+
+
+    private void populateCalendar(ArrayList<DateItem> dateItems) {
+        // Clear all except first row
+        gridPane.getChildren().removeIf(node -> {
+            Integer rowIndex = GridPane.getRowIndex(node);
+            return rowIndex != null && rowIndex > 0;
+        });
+
+        int firstDay = dateItems.get(0).getDay();
+        int lastDay = dateItems.get(dateItems.size() - 1).getDay();
+        for (int i = 0; i < 7 * 6; i++) {
+            int x = i % 7;
+            int y = i / 7 + 1;
+
+            Pane pane = new Pane();
+            pane.getStyleClass().add("cal-item");
+
+            GridPane.setHgrow(pane, javafx.scene.layout.Priority.ALWAYS);
+            GridPane.setVgrow(pane, javafx.scene.layout.Priority.ALWAYS);
+            GridPane.setMargin(pane, new Insets(1));
+
+            if (i < firstDay - 1 || i > lastDay - 1) {
+                gridPane.add(pane, x, y);
+                continue;
+            }
+
+            int day = dateItems.get(i - firstDay + 1).getDay() - firstDay + 1;
+            String info = dateItems.get(i - firstDay + 1).getInfo();
+
+            VBox vBox = new VBox();
+            vBox.prefWidthProperty().bind(pane.widthProperty());
+            vBox.prefHeightProperty().bind(pane.heightProperty());
+
+            Label dayLabel = new Label(String.valueOf(day));
+            dayLabel.prefWidthProperty().bind(pane.widthProperty());
+            dayLabel.prefHeightProperty().bind(pane.heightProperty().divide(4));
+
+            dayLabel.setAlignment(Pos.CENTER);
+
+            Label infoLabel = new Label(info);
+            infoLabel.prefWidthProperty().bind(pane.widthProperty());
+            infoLabel.prefHeightProperty().bind(pane.heightProperty());
+            infoLabel.setAlignment(Pos.TOP_CENTER);
+
+            vBox.getChildren().add(dayLabel);
+            vBox.getChildren().add(infoLabel);
+
+
+            pane.getChildren().add(vBox);
+            gridPane.add(pane, x, y);
+        }
+    }
+}
+
+class DateItem {
+    private final int day;
+    private final String info;
+
+    public DateItem(int day, String info) {
+        this.day = day;
+        this.info = info;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getInfo() {
+        return info;
     }
 }
